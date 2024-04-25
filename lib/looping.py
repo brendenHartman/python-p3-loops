14a72e691aeddbcf2f48066d89dfa78db8a0becd
Brenden Hartman <brendenhart22@gmail.com>
2024-04-25 13:26:44 -0500

    Practiced python loops including list comprehensions and while

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [int * int for int in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i < 101:
+        i_3 = i % 3 == 0
+        i_5 = i % 5 == 0
+        if i_3 and i_5:
+            print("FizzBuzz")
+        elif i_3:
+            print("Fizz")
+        elif i_5: 
+            print("Buzz")
+        else:
+            print(i)
+        i += 1
